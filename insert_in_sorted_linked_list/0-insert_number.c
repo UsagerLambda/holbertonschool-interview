329086e6c5eebde043d1b3f9423751c476125486
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-07 09:18:26 +0200

    add 0-insert_number.c

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * insert_node - Insert a node in ascending order based on number value
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in inserted node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current;  /* Prend le node actuel*/
+	listint_t *next;  /* Prend le prochain node */
+	listint_t *new;  /* Variable pour un nouveau node*/
+
+	current = *head;  /* positionne current sur la tête de la chaine de nœud */
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)  /* Vérifie si ça a échoué */
+		return (NULL);  /* si oui retourne NULL */
+
+	new->n = number; /* assigne au champ n de new notre valeur */
+	new->next = NULL; /* Garde le champ next de new vide pour le moment */
+
+	while (current != NULL)
+	{
+		next = current->next; /* Récupère le prochain node */
+		if (next == NULL) /* Vérifie le prochain node */
+			return (NULL);
+
+		/* Si la valeur du prochain node est supérieur à number */
+		if (next->n > number)
+		{
+			current->next = new; /* notre prochain next deviens new */
+			/* le prochain node de new deviens notre ancien next */
+			new->next = next;
+			return (new);
+		}
+		current = next; /* passe au prochain node */
+	}
+	return (NULL);
+}
