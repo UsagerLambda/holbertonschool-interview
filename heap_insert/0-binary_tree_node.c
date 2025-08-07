326803fb2c317f5074dec6c475b7a97839beabeb
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-07 15:48:19 +0200

    fix 0-binary_tree_node.c by assign NULL to the children of the new node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -17,6 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
 
 	if (new->parent == NULL)
 	{
