c06b7ea64b754b55b2e7fb86097b49ef7de214ec
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-07 15:29:52 +0200

    add 0-binary_tree_node.c

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - Insert a node in a binary tree with a specific value
+ * @parent: parent of the node to create
+ * @value: value of the node to create
+ * Return: address of the new element or NULL if it fails
+**/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	(void)parent;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (new->parent == NULL)
+	{
+		new->parent = new;
+	}
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
