27d09947b67c72e4a85082a7e37f99f22234b037
Utilisateur Lambda <clement.chassem@gmail.com>
2025-09-03 17:13:54 +0200

    refactor 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Analyse les logs et affiche des statistiques."""
-import fileinput
-import re
+import sys
 
-pattern = r'^(\d{1,3}\.){3}\d{1,3}\s+-\s+\[[^\]]+\]\s+"GET\s+/projects/260\s+HTTP/1\.1"\s+(\d{3})\s+(\d+)$'
 total_size = 0
 status_counts = {
     "200": 0, "301": 0, "400": 0, "401": 0,
@@ -24,18 +22,20 @@ def print_stats():
 if __name__ == "__main__":
     line_count = 0
     try:
-        for line in fileinput.input():  # Pour chaque ligne reçu
-            line = line.strip()
-            # Vérifie la correspondance avec le regex
-            match = re.match(pattern, line)
-            if match:  # Si ça match
-                status_code = match.group(2)  # Récupère le code de status
-                file_size = int(match.group(3))  # Récupère le poid du fichier
+        for line in sys.stdin:  # Pour chaque ligne reçu
+            line_count += 1
+            try:
+                parts = line.strip().split()
+                status_code = parts[-2]  # Récupère le code de status
+                file_size = int(parts[-1])  # Récupère le poid du fichier
                 total_size += file_size  # cumul les poids
                 # ajoute 1 lorsqu'un code du dictionnaire est trouvé
                 if status_code in status_counts:
                     status_counts[status_code] += 1
-            line_count += 1  # Compte le nombre de lignes traitées
+
+            except (IndexError, ValueError):
+                continue
+
             if line_count % 10 == 0:  # Tous les 10 lignes traitées
                 print_stats()  # Imprime les stats
     except KeyboardInterrupt:  # CTRL + C
