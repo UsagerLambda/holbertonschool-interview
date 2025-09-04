9c86ba4ced5cceafafae6b848f63c0589e3b5ac0
Utilisateur Lambda <clement.chassem@gmail.com>
2025-09-04 10:31:16 +0200

    implement Floyd algo in 0-check_cycle.c

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -15,20 +15,23 @@ int check_cycle(listint_t *list)
 
 	listint_t *head = list; /* sauvegarde ma tête de la liste */
 
-	listint_t *current = head; /* Créer une tête de parcourt de liste */
+	listint_t *tortue = head;
+	listint_t *lievre = head;
 
 	int H = 1; /* Création d'une variable qui servira de bool*/
 
 	while (H == 1) /* tant que H = 1 */
 	{
-		current = current->next; /* Avance dans la liste */
-		if (current == head) /* Vérifie si current est sur la tête de liste */
-		{ /* Si oui */
-			H = 0; /* coupe la boucle */
-			return (1); /* renvoie 1 (c'est une liste avec un cycle)*/
+		tortue = tortue->next;
+		lievre = lievre->next;
+		lievre = lievre->next;
+		if (tortue == lievre)
+		{
+			H = 0;
+			return (1);
 		}
-		if (current == NULL) /* Si current se heurte à un NULL */
-			return (0); /* Renvoie 0 (car il n'y a pas de cycle)*/
+		if (lievre == NULL)
+			return (0);
 	}
 	return (1);
 }
