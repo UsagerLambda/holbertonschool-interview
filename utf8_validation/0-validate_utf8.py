0a10c9ab09220b1f0691804fd74f6a3b559302e9
Utilisateur Lambda <clement.chassem@gmail.com>
2025-10-10 10:23:28 +0200

    add 0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -14,19 +14,21 @@ def validUTF8(data):
     remaining_bytes = 0
 
     for byte in data:
+        byte = byte & 0xFF
+
         if remaining_bytes == 0:
-            if byte >> 7 == 0b0:
+            if byte >> 7 == 0b0:  # Si on supprime 7 bits à droite
                 remaining_bytes = 0
-            elif byte >> 5 == 0b110:
+            elif byte >> 5 == 0b110:  # 5 bits
                 remaining_bytes = 1
-            elif byte >> 4 == 0b1110:
+            elif byte >> 4 == 0b1110:  # 4 bits
                 remaining_bytes = 2
-            elif byte >> 3 == 0b11110:
+            elif byte >> 3 == 0b11110:  # 3 bits
                 remaining_bytes = 3
             else:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if byte >> 6 != 0b10:  # Si suppr 6 bits à droite ne donne pas 0b10
                 return False
             remaining_bytes -= 1
 
