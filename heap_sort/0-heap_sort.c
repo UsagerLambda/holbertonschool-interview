f7192b1f3f69f3bb94b446165ce845bfb923a939
Utilisateur Lambda <clement.chassem@gmail.com>
2025-12-14 10:28:50 +0100

    fix betty Heap sort

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,42 +1,67 @@
 #include "sort.h"
 #include <stddef.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+/**
+ * swap - Swaps two integers
+ * @a: First integer pointer
+ * @b: Second integer pointer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
 }
 
-void sift_down(int *array, size_t size, size_t i, size_t t_size) {
-    size_t largest = i;
-    size_t left = 2 * i + 1;
-    size_t right = 2 * i + 2;
+/**
+ * sift_down - Maintains the heap property
+ * @array: Array to heapify
+ * @size: Current size of heap
+ * @i: Current node index
+ * @t_size: Total size of array
+ */
+void sift_down(int *array, size_t size, size_t i, size_t t_size)
+{
+	size_t largest = i;
+	size_t left = 2 * i + 1;
+	size_t right = 2 * i + 2;
 
-    if (left < size && array[left] > array[largest])
-        largest = left;
+	if (left < size && array[left] > array[largest])
+		largest = left;
 
-    if (right < size && array[right] > array[largest])
-        largest = right;
+	if (right < size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != i) {
-        swap(&array[i], &array[largest]);
-        print_array(array, t_size);
-        sift_down(array, size, largest, t_size);
-    }
+	if (largest != i)
+	{
+		swap(&array[i], &array[largest]);
+		print_array(array, t_size);
+		sift_down(array, size, largest, t_size);
+	}
 }
 
-void heap_sort(int *array, size_t size) {
-    if (!array || size < 2)
-        return;
+/**
+ * heap_sort - Sorts an array using heap sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void heap_sort(int *array, size_t size)
+{
+	int last_parent, i;
 
-    int last_parent = (size / 2) - 1;
-    
-    for (int i = last_parent; i >= 0; i--)
-        sift_down(array, size, i, size);
+	if (!array || size < 2)
+		return;
 
-    for (size_t i = size - 1; i > 0; i--) {
-        swap(&array[0], &array[i]);
-        print_array(array, size);
-        sift_down(array, i, 0, size);
-    }
+	last_parent = (size / 2) - 1;
+
+	for (i = last_parent; i >= 0; i--)
+		sift_down(array, size, i, size);
+
+	for (i = size - 1; i > 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		sift_down(array, i, 0, size);
+	}
 }
