5ffc0983866c34ec47ed4cde2185cf3688b6392b
Utilisateur Lambda <clement.chassem@gmail.com>
2026-01-31 10:54:53 +0100

    add 0-rotate_2d_matrix.py

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Rotate a 2D matrix in-place."""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix in-place."""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for row in matrix:
+        row.reverse()
