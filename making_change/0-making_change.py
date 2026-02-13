43a85d65aa2d4676f015b9c0727c82197513f2e2
Utilisateur Lambda <clement.chassem@gmail.com>
2026-02-13 13:39:18 +0100

    fix 0-making_change.py

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -16,19 +16,22 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    target = total
-    count = 0
+    # Créer un tableau d'infini positif de taille total
+    dp = [float("inf")] * (total + 1)
+    dp[0] = 0
 
-    while target != 0:
-        candidates = [v for v in coins if v < target]
-        value = max(candidates) if candidates else None
-        if value is not None:
-            target -= value
-            count += 1
-        else:
-            break
+    # Pour chaque montant de 1 à total
+    for amount in range(1, total + 1):
+        # Pour chaque pièces dans la liste de pièces
+        for coin in coins:
+            # Combien de pièces pour chaque valeur jusqu'à amount[total]
+            # coin <= amount la pièce ne dépasse pas le montant
+            # dp[amount - coin] != float('inf') on sait résoudre
+            # les amount précédents
+            if coin <= amount and dp[amount - coin] != float("inf"):
+                # Prend le nombre minimal de pièces nécessaires pour combler
+                # amount - coin
+                # (+ 1 pour la pièce rajoutée à l'itération actuelle)
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    if target != 1:
-        return -1
-
-    return count
+    return dp[total] if dp[total] != float("inf") else -1
